0fc6721b751e4f1f72e389f631270aed8ccd3d29
kaushal <kaushalkumar02918@gmail.com>
2024-06-13 13:46:55 +0530

    fix env

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -62,6 +62,7 @@ function MyState(props) {
         count:0,
         userId: user ? user : "1",
       });
+      console.log(import.meta.env.VITE_BASE_URL);
       let s = `${import.meta.env.VITE_BASE_URL}/${uniqueId}`;
       setLoader(false);
       setShortLink(s);
